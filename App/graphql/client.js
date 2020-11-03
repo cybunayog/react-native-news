498a2cd9ab3d60a68bc510cad8c62d2463fbc017
Cy Bunayog <cybunayog@gmail.com>
2020-11-02 18:36:14 -0800

    Making a Request with Apollo
    PLEASE make sure to use @apollo/client and apollo-link-rest@v0.7.3

--- App/graphql/client.js
+++ App/graphql/client.js
@@ -1,16 +1,16 @@
-import { ApolloClient } from 'apollo-client';
+import { ApolloClient } from '@apollo/client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { RestLink } from 'apollo-link-rest';
 import { API_KEY } from '@env';
 
 const restLink = new RestLink({
-  uri: 'https//newsapi.org/v2/',
+  uri: 'https://newsapi.org/v2/',
   headers: {
     Authorization: API_KEY,
   }
-});
+}); 
 
-export const client = ApolloClient({
+export const client = new ApolloClient({
   link: restLink,
   cache: new InMemoryCache(),
 });
